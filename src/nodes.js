362e7ae7c3c4b15e9d18fb7a35dd37d5e9a10872
Zajith Corro <zajithcorrov@hotmail.com>
2022-06-21 22:28:49 -0500

    [feat]: Add nodes from the page

---
+++ src/nodes.js
@@ -0,0 +1,23 @@
+import { $ } from './utils.js';
+
+export const nodes = {
+	headerSection: $('#header'),
+	trendingPreviewSection: $('#trendingPreview'),
+	categoriesPreviewSection: $('#categoriesPreview'),
+	genericSection: $('#genericList'),
+	movieDetailSection: $('#movieDetail'),
+	searchForm: $('#searchForm'),
+	trendingMoviesPreviewList: $('.trendingPreview-movieList'),
+	categoriesPreviewList: $('.categoriesPreview-list'),
+	movieDetailCategoriesList: $('#movieDetail .categories-list'),
+	relatedMoviesContainer: $('.relatedMovies-scrollContainer'),
+	headerTitle: $('.header-title'),
+	arrowBtn: $('.header-arrow'),
+	headerCategoryTitle: $('.header-title--categoryView'),
+	searchFormInput: $('#searchForm input'),
+	searchFormBtn: $('#searchBtn'),
+	trendingBtn: $('.trendingPreview-btn'),
+	movieDetailTitle: $('.movieDetail-title'),
+	movieDetailDescription: $('.movieDetail-description'),
+	movieDetailScore: $('.movieDetail-score'),
+};
