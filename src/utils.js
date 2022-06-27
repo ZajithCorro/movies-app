8364f4d77024cfa6212e9eb0af32f369f5117081
Zajith Corro <zajithcorrov@hotmail.com>
2022-06-26 22:17:08 -0500

    [feat] Logic to render categories

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,7 @@ export function createMovies(movies, container) {
 	movies.forEach((movie) => {
 		const movieContainer = document.createElement('div');
 		movieContainer.classList.add('movie-container');
+		movieContainer.setAttribute('data-id', movie.id);
 
 		const movieImg = document.createElement('img');
 		movieImg.classList.add('movie-img');
@@ -16,5 +17,36 @@ export function createMovies(movies, container) {
 
 		movieContainer.appendChild(movieImg);
 		container.appendChild(movieContainer);
+	});
+
+	container.addEventListener('click', (e) => {
+		const { target } = e;
+		const { id } = target.closest('.movie-container').dataset;
+
+		location.hash = `#movie=${id}`;
+	});
+}
+
+export function createCategories(categories, container) {
+	container.innerHTML = '';
+	categories.forEach((category) => {
+		const categoryContainer = document.createElement('div');
+		const categoryColor = document.createElement('span');
+		const categoryTitle = document.createElement('p');
+		const categoryTitleText = document.createTextNode(category.name);
+		const classCategory = `category-genre-${category.id}`;
+
+		categoryContainer.classList.add('category-container');
+		categoryTitle.classList.add('category-title');
+		categoryColor.classList.add(classCategory);
+
+		categoryContainer.addEventListener('click', () => {
+			location.hash = `#category=${category.id}&${category.name.replaceAll(' ', '-')}`;
+		});
+
+		categoryTitle.appendChild(categoryTitleText);
+		categoryContainer.appendChild(categoryColor);
+		categoryContainer.appendChild(categoryTitle);
+		container.appendChild(categoryContainer);
 	});
 }
