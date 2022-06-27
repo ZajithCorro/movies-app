d139b0bfca9abab67d7e41e4d27842f90f9caae1
Zajith Corro <zajithcorrov@hotmail.com>
2022-06-26 21:07:01 -0500

    [feat] Add logic to trending list

--- src/trends.js
+++ src/trends.js
@@ -1,6 +1,20 @@
 import { nodes } from './nodes.js';
+import { api } from './axios.js';
+import { createMovies } from './utils.js';
 
 export function trends() {
+	renderUI();
+	getTrendings();
+}
+
+async function getTrendings() {
+	const { data } = await api(`trending/movie/day`);
+	const movies = data.results;
+
+	createMovies(movies, nodes.genericSection);
+}
+
+function renderUI() {
 	nodes.headerSection.classList.remove('header-container--long');
 	nodes.headerSection.style.background = '';
 	nodes.arrowBtn.classList.remove('inactive');
@@ -8,6 +22,7 @@ export function trends() {
 	nodes.headerTitle.classList.add('inactive');
 	nodes.headerCategoryTitle.classList.remove('inactive');
 	nodes.searchForm.classList.add('inactive');
+	nodes.headerCategoryTitle.textContent = 'Trending';
 
 	nodes.trendingPreviewSection.classList.add('inactive');
 	nodes.categoriesPreviewSection.classList.add('inactive');
